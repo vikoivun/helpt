ffd65ae1359348399a68e410e7cdbf5df6958eb0
Timo Tuominen <tituomin@gmail.com>
2016-09-08 15:52:51 +0300

    Support (un)deleting entries.

--- hours/models.py
+++ hours/models.py
@@ -3,11 +3,17 @@ from django.db import models
 
 
 class Entry(models.Model):
+    STATES = (
+        ('public', 'public'),
+        ('deleted', 'deleted'),
+    )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, db_index=True,
                              related_name='entries')
     date = models.DateField(db_index=True)
     task = models.ForeignKey('projects.Task', db_index=True, related_name='entries')
     minutes = models.PositiveIntegerField()
+
+    state = models.CharField(max_length=20, choices=STATES, default='public')
 
     def __str__(self):
         return "{}: {:2f}h on {} by {}".format(self.date, self.minutes / 60.0,
